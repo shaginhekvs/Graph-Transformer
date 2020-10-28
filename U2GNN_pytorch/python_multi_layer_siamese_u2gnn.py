a34796d5bbdf3e3df2e82513881367441f7304ec
shaginhekvs <shaginhekvs@gmail.com>
2020-10-28 14:29:17 +0100

    added fix

--- U2GNN_pytorch/python_multi_layer_siamese_u2gnn.py
+++ U2GNN_pytorch/python_multi_layer_siamese_u2gnn.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from .layers import TransformerEncoderLayerSmaller
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
-from .sampled_softmax import  SampledSoftmax
+#from .sampled_softmax import  SampledSoftmax
 from .sampled_neighbor import SampledNeighbor
 from .pytorch_U2GNN_UnSup import TransformerU2GNN
 from .contrastive_loss import GraphContrastiveLoss
